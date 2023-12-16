cb44e3ede52eec9d11ac9b09b170d7963cd98ba8
Oleksii <103183090+Vlasvin@users.noreply.github.com>
2023-12-16 16:41:13 +0200

    builder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { ContactForm } from "components/ContactForm/ContactForm";
-import { ContactList } from "components/ContactList/ContactList";
+// import { ContactList } from "components/ContactList/ContactList";
 import { Filter } from "components/Filter/Filter";
 import { Div } from "components/ContactForm/ContactForm.styled";
 
